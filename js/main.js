ea741a2eb0175bb886054cfcc80a793d48baea42
j4vokhir.abdurakhmonov <j4vokhir.abdurakhmonov@yandex.ru>
2022-03-18 05:32:17 +0500

    comit

--- js/main.js
+++ js/main.js
@@ -9,3 +9,19 @@ hamburger.addEventListener('click', () => {
 closeBtn.addEventListener('click', () => {
   menu.classList.remove('mobile_menu--on');
 })
+
+
+const barcelona__btn = document.querySelector('.barcelona__btn');
+const mobile_quetion = document.querySelector('.mobile_quetion');
+const curtain = document.querySelector('.curtain');
+const main = document.querySelector('.main');
+
+barcelona__btn.addEventListener('click', () => {
+  mobile_quetion.classList.add('mobile_quetion--on');
+  curtain.classList.add('curtain--on');
+})
+
+curtain.addEventListener('click', () => {
+  mobile_quetion.classList.remove('mobile_quetion--on');
+  curtain.classList.remove('curtain--on');
+})
